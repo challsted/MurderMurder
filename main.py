692b072798e4d57369b26b8a7639e7264d6d559e
hackinginformation <chase.hallsted2012@gmail.com>
2015-04-27 18:53:43 -0500

    A place to store functions that will be used multiple times thoughout the game with out cluttering up the rest of the code

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sys
+from distutils.util import strtobool
+
+"""
+Taken from SO: /questions/3041986/python-command-line-yes-no-input
+Converts y to True
+Converts n to False
+"""
+def usr_yn(question):
+    sys.stdout.write('%s [y/n]\n' % question)
+    while True:
+        try:
+            return strtobool(raw_input().lower())
+        except ValueError:
+            sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
+
+"""
+A way to tell players they are dead, and why they died on a new line!
+Also, a way for them to exit the script OR to continue playing again!
+This needs to function well, players will be seeing this alot!
+# Wondering if i should break Continue into a seperate section #
+"""
+def dead(why):
+    print why, "Congratz! You Are Dead!\n"
+    usr_yn("Would you like to try again?")
+    if True == "y" or "yes":
+        start_bedroom()
+    elif False == "n" or "no":
+        exit(0)
+    else:
+        print "Im going to assume thats a yes"
+        start_bedroom()
+
+"""
+Asks the user to press enter before continueing,
+Partially Taken from SO: /questions/3084508/clear-terminal-in-python
+Should work in both Py2.X and 3.x
+\x1b[ starts a command
+2J clears the screen
+H (with no args) returns the cursor to the top left
+# Port this to other OS's? This is only suppored in ANSI #
+"""
+def clear():
+    raw_input("Press Enter To Continue")
+    sys.stderr.write("\x1b[2J\x1b[H")
