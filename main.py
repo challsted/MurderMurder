aaed404e3f6ce4ea2128ef4c31dcaef3776da333
hackinginformation <chase.hallsted2012@gmail.com>
2015-04-27 23:17:42 -0500

    Having issues with the yes/no function

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 
 import sys
-from distutils.util import strtobool
 
 """
 Taken from SO: /questions/3041986/python-command-line-yes-no-input
 Converts y to True
 Converts n to False
 """
+### THIS WHOLE SECTION IS BROKEN... and i dont know why
 def usr_yn(question):
     sys.stdout.write('%s [y/n]\n' % question)
-    while True:
-        try:
-            return strtobool(raw_input().lower())
-        except ValueError:
-            sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
-
+    choice = raw_input().lower
+    yes = ["y",'yes','ye','']
+    no = ['n','no']
+    if choice in yes:
+        return True
+    if choice in no:
+        return False
+    else:
+        sys.stdout.write("Please respond with 'y'es or 'n'o\n")
 """
 A way to tell players they are dead, and why they died on a new line!
 Also, a way for them to exit the script OR to continue playing again!
@@ -36,10 +39,7 @@ Asks the user to press enter before continueing,
 Partially Taken from SO: /questions/3084508/clear-terminal-in-python
 Should work in both Py2.X and 3.x
 \x1b[ starts a command
-2J clears the screen
-H (with no args) returns the cursor to the top left
-# Port this to other OS's? This is only suppored in ANSI #
-"""
+2J clears the screen H (with no args) returns the cursor to the top left # Port this to other OS's? This is only suppored in ANSI # """
 def clear():
     raw_input("Press Enter To Continue")
     sys.stderr.write("\x1b[2J\x1b[H")
