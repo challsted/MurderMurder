a683c5bd4a0e1c0e1329f88010bf97a4a9989088
hackinginformation <chase.hallsted2012@gmail.com>
2015-04-27 23:18:05 -0500

    added a good amount of stuff here, fought with yes/no function for a while

--- starthere.py
+++ starthere.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python
 
 from main import *
+import random
 
 # Need a place to start, lets try starting here!
 def start_bedroom():
+    awake = 0
+    fear = 0
+    baseballbat = False
+    clear()
     print "Welcome to MurderMurder by MaSammich aka HackingInformation on GitHub" #Gretiem"#, and FullOverRide"
     clear()
     print "Your sleeping in your bed...."
     print "Dreaming of gumdrops, sugar pops, and a lion named Dandy..."
     print "Oh, what sweet dreams your precious mind ponders"
     clear()
-
-def bedroom_p2(awake, fear):
     print "You slightly hear a car door outside of your window, the slam of the door penetrates your sweet dream"
     print "Do you want to investigate or keep sleeping, who knows what you will dream of next?"
     usr_yn("Keep Sleeping?")
 
-    if True:
+    if usr_yn(True):
         print "You drift back to sleep, who cares if someone shut their door too hard"
-    else:
+    elif usr_yn(False):
         print "You lean over and peak out the window next to your bed,\n you see a old truck in the distance, only slightly illuminated by the moonlight"
         print "Its not on your property, no one seems to be near the truck, you pay it no mind"
+    else:
+        print "This should not be seen!"
+
         awake += 1
 
     clear()
@@ -47,36 +53,81 @@ def bedroom_p2(awake, fear):
         else:
             print "Its probably just some stray dog walking though the yard, who cares right?"
 
-    return (awake, fear)
-
-def bedroom_p3(awake, fear):
-    # Checks the alertness of the player from the previous function
-    awake, fear = bedroom_p2(awake, fear)
-
     if awake == 3:
         print "You hear glass shattering from some room down stairs and a thud on the ground"
         print "You turn around from your window to face the sound, but only see darkness"
         print "\"What was that?\" you wonder to your self slightly outloud, shushing your self for not only thinking it"
         print "Your wife jerks awake and is looking around the room frantically for your consolation, you wisper \"shhhhh\" you say to her"
+
         fear += 1
+        chance = random.random()
+
+        if chance >= 0.0 <= 0.89:
+            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
+            if True:
+                print "You bend over and grab the Baseball Bat that you always keep right under your bed"
+                baseballbat = True
+                fear -= 1
+            else:
+                print "Na, that wont help you will it? I mean why would you ever need to defend your self?"
+        else:
+            clear()
+        clear()
     elif awake == 2:
         print "You hear glass shatter from somewhere, your mind franticly decides its coming from somewhere downstairs"
         print "Your wife awakens with a startle and grabs your arm as she sits up"
         print "\"What the fuck was that\" she says terrified"
         print "\"I dont know... it sounded like glass being broken"
+
         fear += 3
+        chance = random.random()
+
+        if chance >= 0.0 <= 0.49:
+            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
+            if True:
+                print "You bend over and grab the Baseball Bat that you always keep right under your bed"
+                baseballbat = True
+                fear -= 1
+            else:
+                print "Na, that wont help you will it? I mean why would you ever need to defend your self?"
+        else:
+            clear()
+        clear()
+
     elif awake == 1:
-        print "You are ripped out of your sleep by the unmistakeable sound of glass shadering"
+        print "You are ripped out of your sleep by the unmistakeable sound of glass shattering"
+        print "Your wife also jolts up and lets out a loud squeek, she grabs you and is shakeing as much as you now are"
+
         fear += 4
+        chance = random.random()
+
+        if chance >= 0.0 <= 0.19:
+            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
+            if True:
+                print "You bend over and grab the Baseball Bat that you always keep right under your bed"
+                baseballbat = True
+                fear -= 1
+            else:
+                print "Na, that wont help you will it? I mean why would you ever need to defend your self?"
+        else:
+            clear()
+        clear()
     else:
         print ""
+
         fear +=5
+        chance = random.random()
 
-
-
-
+        if chance >= 0.0 <= 0.01:
+            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
+            if True:
+                print "You bend over and grab the Baseball Bat that you always keep right under your bed"
+                baseballbat = True
+                fear -= 1
+            else:
+                print "Na, that wont help you will it? I mean why would you ever need to defend your self?"
+        else:
+            clear()
+        clear()
 
 start_bedroom()
-bedroom_p2()
-bedroom_p3()
-
