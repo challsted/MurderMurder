d9c5090970ac6bb64f56bde4e00691faaa8c0318
hackinginformation <chase.hallsted2012@gmail.com>
2015-04-28 19:02:24 -0500

    Converted usr_yn to choice, added breaks to keep screen text to a minimum

--- starthere.py
+++ starthere.py
@@ -9,7 +9,7 @@ def start_bedroom():
     fear = 0
     baseballbat = False
     clear()
-    print "Welcome to MurderMurder by MaSammich aka HackingInformation on GitHub" #Gretiem"#, and FullOverRide"
+    print "Welcome to MurderMurder by HackingInformation(MaSammich) and FullOverRide" #Gretiem"#,
     clear()
     print "Your sleeping in your bed...."
     print "Dreaming of gumdrops, sugar pops, and a lion named Dandy..."
@@ -17,31 +17,27 @@ def start_bedroom():
     clear()
     print "You slightly hear a car door outside of your window, the slam of the door penetrates your sweet dream"
     print "Do you want to investigate or keep sleeping, who knows what you will dream of next?"
-    usr_yn("Keep Sleeping?")
+    choice=raw_input("Keep Sleeping? [y/n]")
+    clear()
 
-    if usr_yn(True):
-        print "You drift back to sleep, who cares if someone shut their door too hard"
-    elif usr_yn(False):
+    if choice == "n":
         print "You lean over and peak out the window next to your bed,\n you see a old truck in the distance, only slightly illuminated by the moonlight"
         print "Its not on your property, no one seems to be near the truck, you pay it no mind"
-    else:
-        print "This should not be seen!"
-
         awake += 1
-
+    else:
+        print "You drift back to sleep, who cares if someone shut their door too hard"
     clear()
 
     if awake == 1:
         print "As your begining to fall back asleep, you begin to hear footsteps in the distance"
+        choice=raw_input("Investigate? [y/n]")
         clear()
-        usr_yn("Investigate?")
-
-        if True:
+        if choice == "y":
             print "You roll over and peak out your window again, you dont see anyone but you faintly hear the sound of footsteps still and not far from you"
             awake += 1
-            usr_yn("Investigate Further?")
+            choice=raw_input("Investigate Further? [y/n]")
 
-            if True:
+            if choice == "y":
                 print "You stand up and take a look out the window, blood begins to rush though your body a little harder as you do"
                 print "You faintly see a shadow slide past the first floor of your house and around a corner"
                 print "You rub your eyes but the shadow is gone, your heart speeds up a little and you can feel your self become fully awake"
@@ -49,29 +45,33 @@ def start_bedroom():
                 fear += 1
             else:
                 print "You lay back in bed, annoyed that you got up to something to simple"
+            clear()
 
         else:
             print "Its probably just some stray dog walking though the yard, who cares right?"
+        clear()
 
     if awake == 3:
         print "You hear glass shattering from some room down stairs and a thud on the ground"
         print "You turn around from your window to face the sound, but only see darkness"
         print "\"What was that?\" you wonder to your self slightly outloud, shushing your self for not only thinking it"
         print "Your wife jerks awake and is looking around the room frantically for your consolation, you wisper \"shhhhh\" you say to her"
+        clear()
 
         fear += 1
         chance = random.random()
 
         if chance >= 0.0 <= 0.89:
-            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
-            if True:
+            choice = raw_input("You remember that you always keep a bat under your bed, should you grab it? [y/n]")
+            if choice == "y":
                 print "You bend over and grab the Baseball Bat that you always keep right under your bed"
                 baseballbat = True
                 fear -= 1
             else:
                 print "Na, that wont help you will it? I mean why would you ever need to defend your self?"
-        else:
             clear()
+        else:
+            print "#####ADD SOMETHING HERE#####"
         clear()
     elif awake == 2:
         print "You hear glass shatter from somewhere, your mind franticly decides its coming from somewhere downstairs"
@@ -83,8 +83,8 @@ def start_bedroom():
         chance = random.random()
 
         if chance >= 0.0 <= 0.49:
-            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
-            if True:
+            choice = raw_input("You remember that you always keep a bat under your bed, should you grab it? [y/n]")
+            if choice == "y":
                 print "You bend over and grab the Baseball Bat that you always keep right under your bed"
                 baseballbat = True
                 fear -= 1
@@ -102,8 +102,8 @@ def start_bedroom():
         chance = random.random()
 
         if chance >= 0.0 <= 0.19:
-            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
-            if True:
+            choice = raw_input("You remember that you always keep a bat under your bed, should you grab it? [y/n]")
+            if choice == "y":
                 print "You bend over and grab the Baseball Bat that you always keep right under your bed"
                 baseballbat = True
                 fear -= 1
@@ -113,14 +113,15 @@ def start_bedroom():
             clear()
         clear()
     else:
-        print ""
+        print "#####ADD SOMETHING HERE#####"
 
         fear +=5
         chance = random.random()
 
         if chance >= 0.0 <= 0.01:
-            usr_yn("You remember that you always keep a bat under your bed, should you grab it?")
-            if True:
+            choice = raw_input("You remember that you always keep a bat under your bed, should you grab it? [y/n]")
+            clear()
+            if choice == "y":
                 print "You bend over and grab the Baseball Bat that you always keep right under your bed"
                 baseballbat = True
                 fear -= 1
